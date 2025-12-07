1ad2255ac1cc6f4afba8a5c633c4fffb826fba33
rexandel <xarendel@gmail.com>
2025-12-08 00:04:05 +0300

    feat: connect adapter to RecyclerView

--- app/src/main/java/com/rexandel/shalli/MainActivity.kt
+++ app/src/main/java/com/rexandel/shalli/MainActivity.kt
@@ -4,16 +4,57 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rexandel.shalli.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val adapter = CityCardAdapter()
+    private val imageIdList = listOf(
+        R.drawable.krasnodar,
+        R.drawable.moscow,
+        R.drawable.saint_petersburg
+    )
+
+    private var index = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        binding.apply {
+            cityWeather.layoutManager = LinearLayoutManager(this@MainActivity)
+            cityWeather.adapter = adapter
+            addCityCard.setOnClickListener {
+                if (index > 2) index = 0
+
+                val cityCard = CityCard(imageIdList[index], "City ${index+1}")
+                adapter.addCityCard(cityCard)
+                index++
+            }
+            cityWeather.setHasFixedSize(true)
+        }
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+            view.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
+
             insets
         }
     }
